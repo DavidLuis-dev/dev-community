d41ca5294f9859647cb1dc73dde5a4add0398937
DavidLuise <108882390+DavidLuise@users.noreply.github.com>
2022-07-23 11:08:48 -0400

    Auth API middleware updated

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,6 +11,7 @@ const User = require('../../models/User');
 // @route    GET api/auth
 // @desc     Get user by token
 // @access   Private
+// @ auth router API handler
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
